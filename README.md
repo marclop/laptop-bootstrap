8ba07400e6f90bf29201b37da189930cc557dfc4
MarcLopez <marc5.12@outlook.com>
2016-01-26 01:42:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Personal bootstrapping for Linux
+
+This repository contains code relevant to bootstrapping my laptop / workstation
+with all that I consider to be essential to be up and running
+
+# Usage
+
+This has one really easy way of using it, and it's Makefile targets.
+
+```bash
+make
+```
---
+++ bootstrap.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+
+#
+# Log functions
+#
+log(){
+  echo "[${1}]: ${*:2}"
+}
+notice(){
+  log "INFO" ${*}
+}
+warning(){
+  log "WARNING" ${*}
+}
+fatal(){
+  log "ERROR" ${*} "Exiting..."
+  exit 1
+}
+
+#
+# Maps the yaml file (Only one depth permitted)
+# to the calling file
+#
+map_environment()
+{
+  local ENV_FILE="${1}"
+  local PARAMETER_LIST=$(grep -v ':$' "${ENV_FILE}" | grep -v '^  - ' | cut -d ':' -f1)
+
+  for PARAMETER in ${PARAMETER_LIST}; do
+    ## Get parameter value
+    local VALUE=$(grep "^${PARAMETER}:" "${ENV_FILE}" | cut -d ':' -f2 | tr -d '\040\011\012\015')
+    local VAR=$(echo ${PARAMETER} | awk '{print toupper($0)}')
+    export ${VAR}="${VALUE}"
+  done
+
+  . VERSIONS
+}
+
+#
+# Checks the supported dists
+#
+supported_dists(){
+  if [[ -z $(which systemctl) ]]; then
+    fatal "Incorrect init system."
+  fi
+  if [[ ! -z $(which lsb_release) ]]; then
+    if [[ $(lsb_release -i -s) != "Ubuntu" ]]; then
+      fatal "Incorrect Linux Distribution."
+    fi
+  else
+    fatal "Incorrect Linux Distribution,"
+  fi
+}
+
+#
+# Set up Git username, Mail and Credential cache
+#
+config(){
+  git config --global user.name "${FULL_NAME}"
+  git config --global user.email "${PERSONAL_EMAIL}"
+  git config --global credential.helper 'cache --timeout=3600'
+}
+
+check_docker(){
+  if [[ -z $(which docker) ]]; then
+    fatal "Docker not installed."
+  fi
+  if [[ $(systemctl is-active docker) != 'active' ]]; then
+    warning "Docker is stopped. Starting it..."
+    sudo systemctl start docker
+  fi
+}
+
+
+#
+# Install docker & docker-compose
+#
+get_docker(){
+  local LOOKUP_FILE="/etc/lsb-release"
+  . ${LOOKUP_FILE}
+  if [[ ! -f "/etc/apt/sources.list.d/docker.list" ]]; then
+    notice "Installing GPG key for Docker repository..."
+    sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+    echo "# ${DISTRIB_ID} ${DISTRIB_CODENAME} ${DISTRIB_RELEASE}
+    deb https://apt.dockerproject.org/repo ubuntu-${DISTRIB_CODENAME} main
+    " > /tmp/docker.list
+    sudo cp /tmp/docker.list /etc/apt/sources.list.d/docker.list
+    rm -f /tmp/docker.list
+    notice "Updating repos..."
+    sudo apt-get update
+    notice "Installing latest Docker-engine version..."
+    sudo apt-get install -y -qq docker-engine
+    notice "Adding current user to docker group..."
+    sudo usermod -aG docker ${USER}
+    notice "Enabling startup Docker daemon..."
+    sudo systemctl enable docker
+    notice "Starting Docker daemon..."
+    sudo service docker start
+    notice "Installing Docker Compose..."
+    curl -sL https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/run.sh -o /tmp/docker-compose
+    sudo cp /tmp/docker-compose /usr/local/bin/ && chmod +x /usr/local/bin/docker-compose
+    rm -f /tmp/docker-compose
+  else
+    check_docker
+    docker images | grep 'docker/compose' | grep ${COMPOSE_VERSION} > /dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+      COMPOSE_PREVIOUS_VERSION=$(docker images | grep 'docker/compose' | awk '{print $2}')
+      notice "Updating Docker Compose..."
+      if [[ ! -z ${COMPOSE_PREVIOUS_VERSION} ]]; then
+        notice "Removing previous version..."
+        docker rmi docker/compose:${COMPOSE_PREVIOUS_VERSION}
+      fi
+      curl -sL https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/run.sh -o /tmp/docker-compose
+      notice "Copying new version to PATH"
+      sudo cp /tmp/docker-compose /usr/local/bin/ && sudo chmod +x /usr/local/bin/docker-compose
+      rm -f /tmp/docker-compose
+    fi
+  fi
+}
+
+#
+# Download and set up Hub cli and alias it as git
+#
+get_hub(){
+  local DEST="/usr/local/bin/"
+  if [[ -f "${DEST}/hub" ]]; then
+    return 0
+  fi
+  notice "Setting up GitHub CLI"
+  local HUB_URL="https://github.com/github/hub/releases/download/v${HUB_VERSION}/hub-linux-amd64-${HUB_VERSION}.tgz"
+  curl -sL ${HUB_URL} | tar xz
+  if [[ -w ${DEST} ]]; then
+    local PREFIX=""
+  else
+    warning "Path not writable by current user, please provide sudo password"
+    local PREFIX="sudo"
+  fi
+  ${PREFIX} mv "hub-linux-amd64-${HUB_VERSION}/bin/hub" ${DEST}
+  rm -rf hub-linux-amd64-*
+}
+
+#
+# Function to add aliases to profile
+#
+create_alias(){
+  local ALIAS_FILE="${HOME}/.bash_aliases"
+  declare -i count=0
+  declare -a KEYS=(ll git)
+  declare -a VALUE=('ls -l' 'hub')
+  for key in ${KEYS[*]}; do
+    grep ${key} ${ALIAS_FILE} > /dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+      alias ${key}="${VALUE[count]}"
+    fi
+    ((count++))
+  done
+  alias >> ${ALIAS_FILE}
+}
+
+####### MAIN EXECUTION BLOCK #######
+
+#
+# Code that runs every function in the script except for helpers
+#
+main(){
+  map_environment environment.yml
+  supported_dists
+
+  for function in ${1}; do
+    ${function}
+  done
+}
+
+EXCLUDES='log|notice|warning|fatal|map_environment|supported_dists|main|check_'
+FUNCTIONS=$(typeset -F | egrep -v ${EXCLUDES} | awk '{print $3}')
+
+main "${FUNCTIONS}"
